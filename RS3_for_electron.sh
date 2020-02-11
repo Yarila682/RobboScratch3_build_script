437eaff6277d98fa4d77d2629aef9d07aedca435
Yarila <yarila682@yandex.ru>
2020-02-11 16:36:25 +0300

    building rs3 on linux; building rs3 for rasberry pi  on linux

---
+++ RS3_for_electron.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets/searching.gif" -sr "./static/robbo_assets/searching.gif"
+#python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets/icon_close_window.png" -sr "./static/robbo_assets/icon_close_window.png" 
+#python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets/icon_close_window_old.svg" -sr "./static/robbo_assets/icon_close_window_old.svg" 
+python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets" -sr "./static/robbo_assets" 
+python3 RS3_electron_rasberry.py -f "lib.min.js" -s "node_fs.readFileSync('settings.json')" -sr "node_fs.readFileSync(electron_app.getAppPath()+'/settings.json')" 
+python3 RS3_electron_rasberry.py -f "lib.min.js" -s 'const getos = require("getos")' -sr 'const electron_app = require("electron").remote.app; const getos = require("getos");' 
