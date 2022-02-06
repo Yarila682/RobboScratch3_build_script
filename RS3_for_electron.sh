fa9325cd9c91aa3546cf3402a377ac79673d8522
yarila682 <yarila682@yandex.ru>
2022-02-06 04:53:26 +0300

    Changes for matching new electron version

--- RS3_for_electron.sh
+++ RS3_for_electron.sh
@@ -4,5 +4,5 @@
 #python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets/icon_close_window.png" -sr "./static/robbo_assets/icon_close_window.png" 
 #python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets/icon_close_window_old.svg" -sr "./static/robbo_assets/icon_close_window_old.svg" 
 python3 RS3_electron_rasberry.py -f "lib.min.js" -s "/build/static/robbo_assets" -sr "./static/robbo_assets" 
-python3 RS3_electron_rasberry.py -f "lib.min.js" -s "node_fs.readFileSync('settings.json')" -sr "node_fs.readFileSync(electron_app.getAppPath()+'/settings.json')" 
-python3 RS3_electron_rasberry.py -f "lib.min.js" -s 'const getos = require("getos")' -sr 'const electron_app = require("electron").remote.app; const getos = require("getos");' 
+#python3 RS3_electron_rasberry.py -f "lib.min.js" -s "node_fs.readFileSync('settings.json')" -sr "node_fs.readFileSync(electron_app.getAppPath()+'/settings.json')" 
+#python3 RS3_electron_rasberry.py -f "lib.min.js" -s 'const getos = require("getos")' -sr 'const electron_app = require("electron").remote.app; const getos = require("getos");' 
